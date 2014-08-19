5962ff8099d137d392f45f17d4260d60c95d7110
AppWerft <rs@hamburger-appwerft.de>
2014-08-19 09:45:55 +0200

        

--- Resources/controls/getrss.js
+++ Resources/controls/getrss.js
@@ -10,7 +10,7 @@ module.exports = function(_key, _callback) {
 				var regex = /"pcast:\/\/(.*?)"/mg;
 				var res = regex.exec(page);
 				if (res) {
-					var url = res[0];
+					var url = res[1];
 					Ti.App.Properties.setString('RSSURL' + key, url);
 					callback(url);
 				}
@@ -21,22 +21,27 @@ module.exports = function(_key, _callback) {
 	}
 
 	getUrl(_key, function(_url) {
+		if (Ti.App.Properties.hasProperty('ITEMS' + _url)) {
+			//callback(Ti.App.Properties.getList('ITEMS' + _url));
+		}
 		var xhr = Ti.Network.createHTTPClient({
 			onload : function() {
-				console.log(this.responseText);
+				console.log(this.responseText.substr(32));
 				Ti.UI.createNotification({
 					message : Math.round(this.responseText.length / 1000) + ' kB'
 				}).show();
 				try {
-					var res = new (require("vendor/XMLTools"))(this.responseXML).toObject();
-					_callback(res.channel.item);
+					var items = new (require("vendor/XMLTools"))(this.responseXML).toObject().channel.item;
+					console.log(items);				
+					Ti.App.Properties.setList('ITEMS' + url, items);
+					_callback(items);
 				} catch(E) {
 					_callback(null);
 					console.log(E);
 				}
 			}
 		});
-		xhr.open('GET', url, true);
+		xhr.open('GET', _url, true);
 		xhr.setRequestHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2');
 		xhr.setRequestHeader('Accept', 'application/rss+xml');
 		xhr.setRequestHeader('Accept-Encoding', 'gzip, deflate');
