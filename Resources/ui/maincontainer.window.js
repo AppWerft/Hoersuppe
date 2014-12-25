946765efe3294995ec00605e6154c920ba9062e4
AppWerft <rs@hamburger-appwerft.de>
2014-12-25 15:35:59 +0100

    flipview for mainview

--- Resources/ui/maincontainer.window.js
+++ Resources/ui/maincontainer.window.js
@@ -6,7 +6,6 @@ module.exports = function(_parent) {
 		backgroundColor : '#aaa',
 	});
 	self.container = Flip.createFlipView({
-	//	backgroundColor : '#fff',
 		top : 0,
 		bottom : 0,
 		orientation : Flip.ORIENTATION_HORIZONTAL,
@@ -19,13 +18,6 @@ module.exports = function(_parent) {
 		Ti.App.Properties.setInt('PEAK', Ti.App.Properties.getInt('PEAK', 0) + 1);
 	});
 	if (Ti.Android) {
-		/*self.addEventListener("open", function() {
-			var activity = self.getActivity();
-			if (activity && activity.actionBar) {
-				activity.actionBar.setTitle('Hörsuppe');
-				activity.actionBar.setSubtitle('deutschsprachige podcasts');
-			}
-		});*/
 		self.container.addEventListener('flipped', function(_e) {
 			var ndx = _e.index;
 			var titles = ['Hörsuppe', 'FrauenPodcasts', 'PodBe'];
